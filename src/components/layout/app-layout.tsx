dc295b8a07b625607a3e4e57b8b70ef9ef08425e
Muhammadamin Anvarov <anvarovmuhammmadamin011@gmail.com>
2026-09-07 19:14:21 +0500

    feat: remove top header from main app layout

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -2,7 +2,6 @@
 
 import type { ReactNode } from "react";
 import { StudentSidebar } from "@/components/layout/sidebar";
-import { Header } from "@/components/layout/header";
 import { MobileNav } from "@/components/layout/mobile-nav";
 import { TelegramWebAppProvider } from "@/components/telegram-webapp-provider";
 
@@ -19,8 +18,6 @@ export function AppLayout({ children }: AppLayoutProps) {
 
       {/* Main Content */}
       <div className="flex flex-1 flex-col overflow-hidden">
-        <Header />
-
         <main className="flex-1 overflow-y-auto pb-20 md:pb-0">
           <div className="container mx-auto p-4 md:p-5 lg:p-6">
             {children}
